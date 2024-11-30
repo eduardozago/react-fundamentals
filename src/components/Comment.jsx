d57cc37e3e787dd1a46f069c93afb2d4900ae96d
Eduardo Zago <zagoe22@gmail.com>
2024-11-29 23:03:59 -0300

    feat: implement comment area

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src='https://plus.unsplash.com/premium_photo-1677545183884-421157b2da02?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt=''/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>John Doe</strong>
+                            <time title='November 28 at 10:20 PM' dateTime='2024-11-28 10:20:30'>
+                                Published at 1h
+                            </time>
+                        </div>
+
+                        <button title='Delete comment'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, est.</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Like <span>5</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
