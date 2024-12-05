15cd44f6b9ce715a2c36fa3ee8b256f2861bb924
Eduardo Zago <zagoe22@gmail.com>
2024-12-05 20:01:43 -0300

    feat: implement comment validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -27,7 +27,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Required field')
     }
 
     function deleteComment(commentToDelete) {
@@ -37,6 +42,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne)
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -71,10 +78,15 @@ export function Post({ author, publishedAt, content }) {
                     placeholder='Leave a comment'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publish</button>
+                    <button 
+                        type='submit' 
+                        disabled={isNewCommentEmpty}
+                    >Publish</button>
                 </footer>
 
             </form>
